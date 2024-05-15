a1242460fe758ab21f9a2d58e61e9cdffd2063f7
Wangui-quinnie <w.wangui@ymail.com>
2024-05-15 03:32:19 -0700

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,58 @@
+// utils/redis.js
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient({
+      host: 'localhost',
+      port: 6379,
+    });
+
+    this.client.on('error', (err) => {
+      console.error('Redis error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
