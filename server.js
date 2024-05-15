882cf5301cdaa105cce42e7a7d4f37bf22ababc8
Wangui-quinnie <w.wangui@ymail.com>
2024-05-15 06:28:26 -0700

    changed the route to index

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // server.js
 const express = require('express');
-const routes = require('./routes');
+const index = require('./routes/index');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // Load routes
-app.use('/', routes);
+app.use('/', index);
+app.use(express.json);
 
 // Start server
 app.listen(PORT, () => {
