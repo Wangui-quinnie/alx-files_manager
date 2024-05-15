1d4015a8355a80fd41d03e484552564b82134f01
Wangui-quinnie <w.wangui@ymail.com>
2024-05-15 03:59:15 -0700

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+// utils/db.js
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url);
+    this.client.connect((err) => {
+      if (err) {
+        console.error('DB connection error:', err);
+      } else {
+        console.log('DB connected successfully');
+      }
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) {
+      throw new Error('DB connection is not alive');
+    }
+    const db = this.client.db();
+    const usersCollection = db.collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) {
+      throw new Error('DB connection is not alive');
+    }
+    const db = this.client.db();
+    const filesCollection = db.collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
