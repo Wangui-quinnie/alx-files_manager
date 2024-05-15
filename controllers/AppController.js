05081fc786ed9eb19385b51a6895d16558840683
Wangui-quinnie <w.wangui@ymail.com>
2024-05-15 06:46:31 -0700

    used functions instead of a class

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,23 +1,20 @@
-// controllers/AppController.js
-const { redisClient, dbClient } = require('../utils');
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
-const AppController = {
-  async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
-  },
-
-  async getStats(req, res) {
-    try {
-      const nbUsers = await dbClient.nbUsers();
-      const nbFiles = await dbClient.nbFiles();
-      res.status(200).json({ users: nbUsers, files: nbFiles });
-    } catch (error) {
-      console.error('Error getting stats:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  },
+const getStatus = (req, res) => {
+  res.status(200).json({
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  });
 };
 
-module.exports = AppController;
+const getStats = async (req, res) => {
+  const nbUsers = await dbClient.nbUsers();
+  const nbFiles = await dbClient.nbFiles();
+  res.status(200).json({
+    users: nbUsers,
+    files: nbFiles,
+  });
+};
+
+module.exports = { getStatus, getStats };
